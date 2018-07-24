16b2c893bb6d34583b25ee05e8d4c30710b38ef0
David Geretti <david.geretti@bestmile.com>
2018-07-24 12:41:42 +0200

    Change card repository to deck manager facade

---
+++ Flashcard/App/DeckManager.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+enum DeckError: Error {
+    case emptyDeck
+}
+
+class DeckManager {
+    private var deck = [Card]()
+    
+    var countCards: Int {
+        return deck.count
+    }
+    
+    func add(card: Card) {
+        deck.append(card)
+    }
+    
+    func cards() -> [Card] {
+        return deck
+    }
+    
+    func draw() throws -> Card {
+        if deck.count <= 0 {
+            throw DeckError.emptyDeck
+        }
+        
+        return deck[0]
+    }
+}
